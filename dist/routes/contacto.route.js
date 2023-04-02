f759a67d97e940ad11a0018dc6bacde3024a4edc
pascualOtuWeb <124534017+pascualOtuWeb@users.noreply.github.com>
2023-04-02 02:23:16 +0200

    Delete dist directory

--- dist/routes/contacto.route.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const contacto_Controllers_1 = require("../controllers/contacto.Controllers");
-//declaracion de la variable
-const router = (0, express_1.Router)();
-router.post('/contacto', contacto_Controllers_1.enviarMensage);
-exports.default = router;
